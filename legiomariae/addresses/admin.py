fe52399a2156a2da17f0f743cc9aad28972ad59c
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-09-12 23:06:43 -0300

    [FEATURE] Added the configuration to application address in settings
    And inserted in the admin section de CRUD for models City, State
    and Country.

--- legiomariae/addresses/admin.py
+++ legiomariae/addresses/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Country, State, City
 
-# Register your models here.
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    pass
