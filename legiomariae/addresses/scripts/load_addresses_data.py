b046fc692a1f71178f9985f0fc5aa54adf417df4
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-09-17 23:33:40 -0300

    [REFACTOR] It was refactore the scripts to populate
    TODO: Finish the script with states and city

--- legiomariae/addresses/scripts/load_addresses_data.py
+++ legiomariae/addresses/scripts/load_addresses_data.py
@@ -15,25 +15,43 @@ def run():
     for country in data:
         if country["id"] == 31:
             brazil = country
-        print(f'{country["id"]} - País:{country["name"]}')
-
+            
     print('Fim da primeira parte')
     if Country.objects.filter(id=31).exists():
-        print(f'Este país já se encontra cadastrado no sistema com o id {country["id"]}!')
+        print(f'Este país já se encontra cadastrado no sistema com o id {31}!')
     else:
         print('Verificando país...')
         if brazil:
-            with transaction.atomic():
-                id = brazil["id"]
-                name = brazil["name"]
-                acronym = brazil["iso3"]
-                code = brazil["numeric_code"]
-                phone_code = brazil["phone_code"]
-
-                new_country = Country(id=id, name=name,
-                                      acronym=acronym, code=code,
-                                      phone_code=phone_code)
-
-                new_country.save()
+            set_country(brazil)
 
     jsonfile.close()
+
+
+def set_country(country):
+    with transaction.atomic():
+        id = country["id"]
+        name = country["name"]
+        acronym = country["iso3"]
+        code = country["numeric_code"]
+        phone_code = country["phone_code"]
+
+        new_country = Country(id=id, name=name,
+                              acronym=acronym, code=code,
+                              phone_code=phone_code)
+
+        new_country.save()
+
+
+def set_state(country_id, state):
+    with transaction.atomic():
+        id = state["id"]
+        name = state["name"]
+        acronym = state["state_code"]
+        code = state["numeric_code"]
+        phone_code = state["phone_code"]
+
+        new_country = Country(id=id, name=name,
+                              acronym=acronym, code=code,
+                              phone_code=phone_code)
+
+        new_country.save()
