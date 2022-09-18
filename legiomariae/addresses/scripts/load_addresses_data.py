1b8181df518707a0b6a79ede39db561f854f39e3
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-09-18 20:33:43 -0300

    [FEATURE] Created script to populate the Brazil Country, State, City models
    It was created the script to populate Brazil's data.
    
    to run the script:
    
    python manage.py runscript load_addresses_data

--- legiomariae/addresses/scripts/load_addresses_data.py
+++ legiomariae/addresses/scripts/load_addresses_data.py
@@ -1,11 +1,10 @@
 import json
 from django.db import transaction
 
-from ..models import Country
+from ..models import Country, State, City
 
 
 def run():
-    print('Iniciando')
     jsonfile = open('addresses/scripts/countries_states_cities.json')
 
     data = json.load(jsonfile)
@@ -15,14 +14,14 @@ def run():
     for country in data:
         if country["id"] == 31:
             brazil = country
-            
-    print('Fim da primeira parte')
+
     if Country.objects.filter(id=31).exists():
         print(f'Este país já se encontra cadastrado no sistema com o id {31}!')
     else:
         print('Verificando país...')
         if brazil:
-            set_country(brazil)
+            # set_country(brazil)
+            set_country_states_cities(brazil)
 
     jsonfile.close()
 
@@ -40,6 +39,7 @@ def set_country(country):
                               phone_code=phone_code)
 
         new_country.save()
+        return new_country
 
 
 def set_state(country_id, state):
@@ -47,11 +47,37 @@ def set_state(country_id, state):
         id = state["id"]
         name = state["name"]
         acronym = state["state_code"]
-        code = state["numeric_code"]
-        phone_code = state["phone_code"]
 
-        new_country = Country(id=id, name=name,
-                              acronym=acronym, code=code,
-                              phone_code=phone_code)
+        new_state = State(id=id,
+                          name=name,
+                          acronym=acronym,
+                          country=country_id)
 
-        new_country.save()
+        new_state.save()
+        return new_state
+
+
+def set_city(state_id, city):
+    with transaction.atomic():
+        id = city["id"]
+        name = city["name"]
+
+        new_city = City(id=id,
+                        name=name,
+                        state=state_id)
+
+        new_city.save()
+        return new_city
+
+
+def set_country_states_cities(country):
+    with transaction.atomic():
+        new_country = set_country(country)
+
+        states = country["states"]
+        for state in states:
+            new_state = set_state(new_country, state)
+
+            cities = state["cities"]
+            for city in cities:
+                set_city(new_state, city)
