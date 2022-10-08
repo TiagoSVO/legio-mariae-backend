e9562fe6875ef3c6684b3e07e3f92784bef713e2
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-10-08 20:12:50 -0300

    [FEATURE] Implementado models referente ao Member
    Adicionado também o NestedAdmin no projeto
    para poder criar no admin o cadastro e atualização
    de models aninhadas.

--- legiomariae/members/admin.py
+++ legiomariae/members/admin.py
@@ -1,25 +1,52 @@
 from django.contrib import admin
-from .models import Member, MemberAddress, MemberPhone
+from .models import Member, MemberAddress, MemberPhone, MemberFiliationType, \
+                    MemberFiliation, PostOffice, PostOfficeMemberFiliation
 from addresses.forms import StackedAddressForm
+from nested_admin import NestedModelAdmin, NestedStackedInline
 
 
-class MemberPhoneInline(admin.StackedInline):
+class MemberPhoneInline(NestedStackedInline):
     model = MemberPhone
     extra = 0
 
 
-class MemberAddressInline(StackedAddressForm):
+class MemberAddressInline(StackedAddressForm, NestedStackedInline):
     model = MemberAddress
     extra = 0
 
 
+class PostOfficeMemberFiliationInline(NestedStackedInline):
+    model = PostOfficeMemberFiliation
+    extra = 0
+    max_num = 1
+
+
+class MemberFiliationInline(NestedStackedInline):
+    model = MemberFiliation
+    extra = 0
+
+    inlines = [PostOfficeMemberFiliationInline,]
+
+
+
 @admin.register(Member)
-class MemberAdmin(admin.ModelAdmin):
+class MemberAdmin(NestedModelAdmin):
     fieldsets = [
         (None, {'fields': ['complete_name', 'cpf', 'nickname', 'birthday', 'email',
                            'deleted']}),
     ]
 
-    inlines = [MemberAddressInline, MemberPhoneInline]
+    inlines = [MemberAddressInline, MemberPhoneInline, MemberFiliationInline]
 
     list_display = ['complete_name', 'nickname', 'birthday', 'deleted']
+
+
+@admin.register(MemberFiliationType)
+class MemberFiliationTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(PostOffice)
+class PostOfficeAdmin(admin.ModelAdmin):
+    pass
+
--- legiomariae/members/models.py
+++ legiomariae/members/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from addresses.models import Address
+from organizations.models import Organization
 from phones.models import Phone
 
 
@@ -45,9 +46,93 @@ class MemberPhone(Phone):
         verbose_name_plural = "Telefoness dos Membros"
 
     def __str__(self):
-        return f'{self.get_name_name} - {self.number_formatted}'
+        return f'{self.get_name} - {self.number_formatted}'
 
     @property
-    def get_name_name(self):
+    def get_name(self):
         return self.member.complete_name if hasattr(self, 'member') else ''
+
+
+class MemberFiliationType(models.Model):
+    code = models.CharField(max_length=3, verbose_name='Código do Tipo de Membro')
+    name = models.CharField(max_length=30, verbose_name='Nome do Tipo de Membro')
+    description = models.TextField(verbose_name="Descrição", null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Tipo do Membro"
+        verbose_name_plural = "Tipos dos Membros"
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
+
+
+class MemberFiliation(models.Model):
+    joined_at = models.DateField(verbose_name='Data de Ingresso')
+    member = models.ForeignKey(Member, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    member_filiation_type = models.ForeignKey(MemberFiliationType, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Filiação do Membro"
+        verbose_name_plural = "Filiações dos Membros"
+
+        constraints = [
+            models.UniqueConstraint(fields=['member', 'organization'], name='unique_member_in_organization'),
+        ]
+
+    def __str__(self):
+        return f'{self.get_member_name} - {self.get_organization_name}'
+
+    @property
+    def get_member_name(self):
+        return self.member.complete_name if hasattr(self, 'member') else ''
+
+    @property
+    def get_organization_name(self):
+        return self.organization.name if hasattr(self, 'organization') else ''
+
+
+class PostOffice(models.Model):
+    code = models.CharField(max_length=3, verbose_name='Código do Tipo de Membro')
+    name = models.CharField(max_length=30, verbose_name='Nome do Tipo de Membro')
+    description = models.TextField(verbose_name="Descrição", null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Cargo de Oficial"
+        verbose_name_plural = "Cargos de Oficiais"
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
+
+
+class PostOfficeMemberFiliation(models.Model):
+    joined_at = models.DateField(verbose_name='Data de Ingresso')
+    member_filiation = models.ForeignKey(MemberFiliation, on_delete=models.CASCADE)
+    post_office = models.ForeignKey(PostOffice, on_delete=models.CASCADE)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Cargo de Oficial do Membro na Organização"
+        verbose_name_plural = "Cargos de Oficiais dos Membros na Organizações"
+
+        constraints = [
+            models.UniqueConstraint(fields=['member_filiation', 'post_office'],
+                                    name='unique_postoffice_to_memberfiliation'),
+        ]
+
+    def __str__(self):
+        return f'{self.code} - {self.name}'
+
+
 
