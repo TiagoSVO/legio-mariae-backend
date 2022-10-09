f22cde26b29dd0a89f24cae5a8828f5ddfd38169
Tiago da Silva Valeriano de Oliveira <tiago.svo@gmail.com>
2022-10-08 23:41:09 -0300

    [FEATURE] Created the model to Meeting

--- legiomariae/meetings/models.py
+++ legiomariae/meetings/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
-# Create your models here.
+from organizations.models import Organization
+
+
+class Meeting(models.Model):
+    date = models.DateField(verbose_name='Data da Reunião')
+    start_at = models.TimeField(verbose_name='Início da Reunião')
+    place_address = models.CharField(max_length=255, verbose_name='Local da Reunião', blank=True, null=True)
+    initial_prayer = models.BooleanField(verbose_name='Orações Iniciais', default=True)
+    rosary_prayer = models.BooleanField(verbose_name='Oração do Terço', default=True)
+    spiritual_read = models.CharField(max_length=255, verbose_name='Leitura Espiritual', blank=True, null=True)
+    standing_instructions_readed = models.BooleanField(verbose_name='Instrução Permanente', default=False)
+    catena_prayer = models.BooleanField(verbose_name='Oração da Catena', default=True)
+    allocutio = models.TextField(verbose_name='Allocutio', blank=True, null=True)
+    announcements = models.TextField(verbose_name='Avisos e Outros Assuntos', blank=True, null=True)
+    final_observations = models.TextField(verbose_name='Observações Finais', blank=True, null=True)
+    final_prayer = models.BooleanField(verbose_name='Oração da Catena', default=True)
+    end_at = models.TimeField(verbose_name='Final da Reunião')
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE, verbose_name='Organização')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "Reunião"
+        verbose_name_plural = "Reuniões"
+
+    def __str__(self):
+        return f'Reunião do dia {self.date} | {self.organization_type_name} {self.organization_name}'
+
+    @property
+    def organization_type_name(self):
+        return f'{self.organization.organization_type.name}'
+
+    @property
+    def organization_name(self):
+        return f'{self.organization.our_blessed_lady_title.name}'
